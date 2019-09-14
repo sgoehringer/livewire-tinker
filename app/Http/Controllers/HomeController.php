fe62aa37cc082a78041347f786d56c6a349ee95d
Scott G <scottg@creationproject.com>
2019-09-14 07:42:33 -0400

    WIP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Log;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        Log::channel('slack-info')->info('Home accessed');
         return view('home');
     }
 }
